4e9eb7efa446d6ef4bc763bbceb7ffa9175fc069
bbreck3 <bbreck3@gmail.com>
2016-03-31 00:37:33 -0400

    starting class website

---
+++ WebSite/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("p1").mouseover(function(){
+        $("p1").css("background-color", "black");
+    });
+    $("p").mouseout(function(){
+        $("p").css("background-color", "lightgray");
+    });
+});
