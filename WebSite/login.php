89e3e54608d3126fa3886e6ae5d2f33891c903ed
bbreck3 <bbreck3@gmail.com>
2016-04-07 17:11:04 -0400

    RAMTunes...WebSite for Database Theory

---
+++ WebSite/login.php
@@ -0,0 +1,22 @@
+
+<?php
+$uname =$_POST['u_name'];
+$password =$_POST['password'];
+
+
+echo nl2br("Debug-->\nPrinting info: \nUserName:  .$uname. \n Password: .$password. \nInformation Proccessed");
+echo nl2br("To impliment:--> \n connect to and querry database \n Security: password using one way hash ");
+
+
+
+
+
+
+
+
+
+
+//header('Location: dashboard.html');
+
+?>
+
